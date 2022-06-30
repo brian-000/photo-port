fdafd3162d0777ba3809ef543d996485dd430c94
Brain <ortiz_brian323@yahoo.com>
2022-06-29 21:48:30 -0700

    added testing for Contact

--- src/components/Nav/__tests__/index.test.js
+++ src/components/Nav/__tests__/index.test.js
@@ -8,45 +8,59 @@ const categories = [
 ]
 const mockCurrentCategory = jest.fn();
 const mockSetCurrentCategory = jest.fn();
+const mockContactSelected = jest.fn();
+const mockSetContactSelected = jest.fn();
 
 afterEach(cleanup);
 describe('Nav component', () => {
-    // baseline test
-    it('renders', () => {
-      render(<Nav
-        categories={categories}
-        setCurrentCategory={mockSetCurrentCategory}
-        currentCategory={mockCurrentCategory}
-      />);
-    })
-  
-    //snapshot test
-    it('matches snapshot', () => {
-        const { asFragment } = render(<Nav categories={categories}
-          setCurrentCategory={mockSetCurrentCategory}
-          currentCategory={mockCurrentCategory}/>);
-        // assert value comparison
-        expect(asFragment()).toMatchSnapshot();
-      });
+  // baseline test
+  it('renders', () => {
+    render(<Nav
+      categories={categories}
+      setCurrentCategory={mockSetCurrentCategory}
+      currentCategory={mockCurrentCategory}
+      contactSelected={mockContactSelected}
+      setContactSelected={mockSetContactSelected}
+
+    />);
   })
 
-  describe('emoji is visible', () => {
-    it('inserts emoji into the h2', () => {
+  //snapshot test
+  it('matches snapshot', () => {
+    const { asFragment } = render(<Nav categories={categories}
+      setCurrentCategory={mockSetCurrentCategory}
+      currentCategory={mockCurrentCategory}
+      contactSelected={mockContactSelected}
+      setContactSelected={mockSetContactSelected}
+    />);
+    // assert value comparison
+    expect(asFragment()).toMatchSnapshot();
+  });
+})
+
+describe('emoji is visible', () => {
+  it('inserts emoji into the h2', () => {
     // Arrange
     const { getByLabelText } = render(<Nav categories={categories}
       setCurrentCategory={mockSetCurrentCategory}
-      currentCategory={mockCurrentCategory}/>);
+      currentCategory={mockCurrentCategory}
+      contactSelected={mockContactSelected}
+      setContactSelected={mockSetContactSelected}
+    />);
     // Assert
     expect(getByLabelText('camera')).toHaveTextContent('📸');
-    })
   })
+})
 
-  describe('links are visible', () => {
-    it('inserts text into the links', () => {
-      const { getByTestId } = render(<Nav categories={categories}
-        setCurrentCategory={mockSetCurrentCategory}
-        currentCategory={mockCurrentCategory}/>);
-      expect(getByTestId('link')).toHaveTextContent('Oh Snap!');
-      expect(getByTestId('about')).toHaveTextContent('About me');
-    });
-  })
+describe('links are visible', () => {
+  it('inserts text into the links', () => {
+    const { getByTestId } = render(<Nav categories={categories}
+      setCurrentCategory={mockSetCurrentCategory}
+      currentCategory={mockCurrentCategory}
+      contactSelected={mockContactSelected}
+      setContactSelected={mockSetContactSelected}
+    />);
+    expect(getByTestId('link')).toHaveTextContent('Oh Snap!');
+    expect(getByTestId('about')).toHaveTextContent('About me');
+  });
+})
